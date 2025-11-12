ce771f1da475b0abe8f98de3027b687607369521
maragubit <maragubit@gmail.com>
2025-11-12 21:56:02 +0100

    emaillist admin

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User,EmailList
 # Register your models here.
 admin.site.site_header = "Queens English Institute – Panel de Administración"
 admin.site.site_title = "Queens English Institute Admin"
@@ -11,3 +11,5 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ('username', 'email', 'first_name', 'last_name')        # Optional: add a search bar
     ordering = ('-date_joined',)             # Optional: default order
     list_editable = ('is_active', 'role')             # Optional: make fields editable directly from the list view
+    
+admin.site.register(EmailList)
